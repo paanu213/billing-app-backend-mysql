4d360606b550fe0034e24585e6c3fcf9c9319b69
paanu213 <panu.ramsetti@gmail.com>
2026-03-24 12:51:27 +0530

    config updated with prod environment details

--- config/config.js
+++ config/config.js
@@ -8,6 +8,19 @@ module.exports = {
         host: process.env.DB_HOST,
         dialect: process.env.DB_DIALECT,
         logging: false
-
+    },
+    production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT || 'mysql',
+    logging: false, // Clean logs in production
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
     }
+}
 }
